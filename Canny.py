e00798885e6792763f582671e7890aab33a6a6b3
root <root@JimmyPC.localdomain>
2025-08-22 16:55:28 -0600

    Primera actualización

--- Canny.py
+++ Canny.py
@@ -1,6 +1,12 @@
+## creado por jaime uriel fonseca mexia
 import cv2
 
 img = cv2.imread('Carta.jpg',0)
 bordeCanny = cv2.Canny(img, 100,200)
 
+cv2.imshow('Original', img)
+cv2.imshow('blur', bordeCanny)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
 
